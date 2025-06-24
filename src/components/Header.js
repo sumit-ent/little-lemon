30fa16196e4bcf0476179bcfcbe52e664e5fbb5c
Sumit Ingole <sumit.ingole@outlook.com>
2025-06-24 17:12:38 +0530

    basic app setup with semantic tags and open graph protocols

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import Nav from "./Nav";
+function Header() {
+  return (
+    <>
+      <header>
+        <img src="/logo.svg" alt="brand logo" />
+        <Nav />
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,5 @@
+function Main() {
+  return <p>main component</p>;
+}
+
+export default Main;
