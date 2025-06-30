709725ecc8b74827041b7c52626e3858864f5fef
Sumit Ingole <sumit.ingole@outlook.com>
2025-06-30 11:31:19 +0530

    specials section implementation

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -4,14 +4,17 @@ function Specials() {
   return (
     <div className="item-container">
       <img className="item-image" src="./menu/lemon-dessert.jpg"></img>
-      <div>
+      <div className="item-spec">
         <span className="item-name">Lemon Desert</span>{" "}
         <span className="item-price">$ 5.99</span>
       </div>
       <div className="item-description">
         A delightful lemon dessert that is both refreshing and satisfying.
       </div>
-      <div className="add-button">Add to Cart</div>
+      <div className="item-action">
+        Order a Delivery
+        <img className="order-image" src="./menu/order-icon.svg"></img>
+      </div>
     </div>
   );
 }
--- src/components/Specials/Specials.js
+++ src/components/Specials/Specials.js
@@ -12,7 +12,6 @@ function Specials() {
         <MenuItem />
         <MenuItem />
         <MenuItem />
-        <MenuItem />
       </div>
     </>
   );
